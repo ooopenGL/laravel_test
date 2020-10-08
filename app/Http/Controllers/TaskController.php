f46b9925b7f8a2dae4a81183a0e4f23cf4e151be
xiajia <xiajia@xiajiadeMacBook-Pro.local>
2020-10-08 22:40:35 +0800

    add taskcontroller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiajia
+ * Date: 2020/9/29
+ * Time: 9:38 PM
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function home()
+    {
+        return 'Hello, World!';
+    }
+}
