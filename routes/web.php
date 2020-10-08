bf09115e529a8fbd6a5a3f5ffe805a68fe5e618c
xiajia <xiajia@xiajiadeMacBook-Pro.local>
2020-10-09 00:11:21 +0800

    添加中间件、兜底路由、method改写、token参数

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,31 @@ Route::get('/', function () {
 });
 
 Route::get('/task', 'TaskController@home');
-Route::resource('post', 'PostController');
+
+// 资源管理器模式路由, 不同的method与url的组合会分别映射到不同的方法上
+//Route::resource('post', 'PostController');
+
+// 兜底路由
+Route::fallback(function () {
+    return '哎，这真是太为难我了～';
+});
+
+//路由中间件配置
+Route::group(['middleware' => ['throttle:6,1']], function () {
+    Route::get('post', 'PostController@index');
+});
+
+// method改写, 表单加入token参数
+Route::get('task/{id}/delete', function ($id) {
+    return '<form method="post" action="' . route('task.delete', [$id]) . '">
+                <input type="hidden" name="_method" value="DELETE"> 
+                <input type="hidden" name="_token" value="' . csrf_token() . '">
+                <button type="submit">删除任务</button>
+            </form>';
+});
+Route::delete('task/{id}', function ($id) {
+    return 'Delete Task ' . $id;
+})->name('task.delete');
+
+
+
